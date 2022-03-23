e5feb0db1a1e5f1722395421cc8dffe651617bba
DamianYongjae <klick214@gmail.com>
2022-03-23 16:32:08 -0400

    2.0 Firebase Auth

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,16 @@
 import AppRouter from "./Router";
+import { useState } from "react";
+import { authService } from "fbase";
 
 function App() {
-  return <AppRouter />;
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+
+  return (
+    <>
+      <AppRouter isLoggedIn={isLoggedIn} />
+      <footer>Twitter Clone {new Date().getFullYear()}</footer>
+    </>
+  );
 }
 
 export default App;
--- src/components/Router.js
+++ src/components/Router.js
@@ -1,13 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "../routes/Home";
 import Auth from "../routes/Auth";
 import Profile from "../routes/Profile";
 import EditProfile from "../routes/EditProfile";
 
-const AppRouter = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
-
+const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
       <Switch>
